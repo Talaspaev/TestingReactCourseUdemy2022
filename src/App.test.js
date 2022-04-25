c335b40d58982a5c20e8c779b9957405f54fda87
mukhamed.talaspaev.epam <mukhamed_talaspaev@epam.com>
2022-04-26 01:23:50 +0300

    test changing color

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("button has correct initial color", () => {
+  //render component
   render(<App />);
-  const colorButton = screen.getByRole("button", { name: "button is blue" });
-  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  // find element with role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  //expect the background- is red
+  expect(colorButton).toHaveStyle("backgroundColor: red");
+
+  //Clik the button
+
+  fireEvent.click(colorButton);
+
+  //expect the background is blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  // check element with text of 'Change to red'
+  expect(colorButton.textContent).toBe("Change to red");
 });
