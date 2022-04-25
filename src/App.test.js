afac25339a3baba71af8d2bb125390faebf491d9
mukhamed.talaspaev.epam <mukhamed_talaspaev@epam.com>
2022-04-26 00:39:40 +0300

    create first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("button has correct initial color", () => {
   render(<App />);
-  const linkElement = screen.getByRole("link", { name: /learn react/i });
-  expect(linkElement).toBeInTheDocument();
+  const colorButton = screen.getByRole("button", { name: "button is blue" });
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
 });
