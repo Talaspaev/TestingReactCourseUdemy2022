2764ddfd90a4a8fec8dfe4c8627793b200ff1f9b
mukhamed.talaspaev.epam <mukhamed_talaspaev@epam.com>
2022-04-26 23:22:19 +0300

    initial conditions for checkbox

--- src/App.test.js
+++ src/App.test.js
@@ -9,7 +9,7 @@ test("button has correct initial color", () => {
   const colorButton = screen.getByRole("button", { name: "Change to blue" });
 
   //expect the background- is red
-  expect(colorButton).toHaveStyle("backgroundColor: red");
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
 
   //Clik the button
 
@@ -20,4 +20,13 @@ test("button has correct initial color", () => {
 
   // check element with text of 'Change to red'
   expect(colorButton.textContent).toBe("Change to red");
+});
+
+test("initial conditions", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  expect(colorButton).toBeEnabled();
+
+  const checkbox = screen.getByRole("checkbox");
+  expect(checkbox).not.toBeChecked();
 });
